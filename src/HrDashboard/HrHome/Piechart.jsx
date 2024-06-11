145eab691099e07a5fa33cdeeb038d1f64b3298b
Jeem312 <Shajidajeem312@gmail.com>
2024-06-11 21:06:06 +0600

    added pie chart

--- src/HrDashboard/HrHome/Piechart.jsx
+++ src/HrDashboard/HrHome/Piechart.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import useAssets from '../../Hooks/useAssets';
+import { Cell, Legend, Pie, PieChart, Tooltip } from 'recharts';
 
 const Piechart = () => {
     const [assets] =useAssets();
@@ -9,10 +10,31 @@ const Piechart = () => {
     const returnable= assets.filter(item=>item.productType==='ReturnAble') ;
     const returnableLength = returnable.length;
     console.log(returnableLength);
+    const data = [
+        { name: 'Returnable', value: returnableLength },
+        { name: 'Non-Returnable', value: nonReturnableLength }
+    ];
+
+    const COLORS = ['#0088FE', '#FF8042'];
     return (
-        <div>
-            
-        </div>
+        <PieChart width={400} height={400}>
+            <Pie
+                data={data}
+                cx={200}
+                cy={200}
+                innerRadius={60}
+                outerRadius={80}
+                fill="#8884d8"
+                dataKey="value"
+                label
+            >
+                {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                ))}
+            </Pie>
+            <Tooltip />
+            <Legend />
+        </PieChart>
     );
 };
 
